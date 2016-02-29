23890973079b1bf151126c140e6fc037a9ba56d9
Tyson Cadenhead <tysoncadenhead@Tysons-Macbook-Pro-2.local>
2016-02-29 16:01:17 -0600

    Removed a bunch of old stale tests

--- test/index.js
+++ test/index.js
@@ -3,3 +3,7 @@ const path = require('path');
 require('questy')({
     rootPath: path.resolve(__dirname, '../examples')
 });
+
+require('questy')({
+    rootPath: path.resolve(__dirname, '../src')
+});
